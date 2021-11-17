5065e36a0d1c66507a3640eb8ab946836a106fe2
fynjn77 <fynjn77@mail.ru>
2021-11-17 18:39:34 +0300

    forget update

---
+++ commands.py
@@ -0,0 +1,38 @@
+# django-admin startproject myfirstproject #создаю проект в консоли
+# cd myfirstproject #захожу в создавшуюся папку
+# py manage.py runserver #запускаю сервер
+# Ctrl+C #остановить сервер
+# py manage.py startapp name_of_app
+# py manage.py migrate
+
+
+
+# pip install virtualenv
+# virtualenv name_of_env
+# name_of_env\Scripts\activate
+# django-admin startproject myfirstproject #Создаю джанго проект
+#на уровне name_of_env
+
+# local
+# git --version #Узнать текущую версию (если git стоит)
+# git init #В папке проекта
+# git status #Покажет, если что-то не отслеживается
+# git add . #добавить все файлы, которые не отслеживаются
+# git commit -m "commit message" #Сохраняю текующее состояние в git
+#и комментарий к сохранению
+# Для загрузки на github нужно на сайте создать репазиторий
+# git remote add origin ссылка_на_репазиторий
+# git branch -M main
+# git push -u origin main # отправить на git
+
+#PostgreSQL
+# \du # посмотреть всех пользователей данной БД
+# \! chcp 1251  # смена кодировки(откракозябр)
+
+
+
+
+
+
+
+
